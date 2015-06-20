5025f636051dd3bdf1fd8eea2ea8b17017cd342d
Steffen Bleul <blacklabelops@itbleul.de>
2015-06-20 20:26:32 +0200

    Fixed path expression.

--- scripts/buildBaseImage.sh
+++ scripts/buildBaseImage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo ~/docker-1.6.2/contrib/mkimage.sh -t blacklabelops/centos rinse --distribution centos-7 --arch amd64
+sudo /home/vagrant/docker-1.6.2/contrib/mkimage.sh -t blacklabelops/centos rinse --distribution centos-7 --arch amd64
 docker save blacklabelops/centos > /vagrant/dockercentos.tar
