87d4fe5c8e0af5c077a47a8341856ba18fee1447
Steffen Bleul <blacklabelops@itbleul.de>
2015-07-03 13:47:38 +0200

    Changed example.

--- scripts/buildBaseImage.sh
+++ scripts/buildBaseImage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo /home/vagrant/docker-1.6.2/contrib/mkimage.sh -t blacklabelops/centos rinse --distribution centos-7 --arch amd64
-docker save blacklabelops/centos > /vagrant/dockercentos.tar
+sudo /home/vagrant/docker-1.6.2/contrib/mkimage.sh -t rinsecentos rinse --distribution centos-7 --arch amd64
+docker save rinsecentos > /vagrant/dockercentos.tar
