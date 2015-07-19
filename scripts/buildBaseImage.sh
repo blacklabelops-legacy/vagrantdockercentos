7bd513296d5d29bd1bb3e97b870391fa67a4d6ac
Steffen Bleul <blacklabelops@itbleul.de>
2015-07-19 21:35:48 +0200

    Building docker base image with systemd.

--- scripts/buildBaseImage.sh
+++ scripts/buildBaseImage.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sudo /home/vagrant/docker-1.6.2/contrib/mkimage.sh -t rinsecentos rinse --distribution centos-7 --arch amd64
-docker save rinsecentos > blacklabelops-centos7-systemd.tar
+docker run -d --name rinsecentos rinsecentos
+docker export rinsecentos |  xz --best >>  blacklabelops-centos7-systemd.xz
